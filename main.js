e0f90d49329e3f3b236755c8a3679478fc2387bd
Matty-El <leachmatthewj@gmail.com>
2021-02-03 08:17:51 +0000

    Completed more lessons

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 var xhr = new XMLHttpRequest();
-
-xhr.onreadystatechange = function() {
-    if (this.readyState = 4&& this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
-    }
-};
+var data;
 
 xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
 
 xhr.send();
+
+xhr.onreadystatechange = function() {
+    if (this.readyState = 4&& this.status == 200) {
+        data = this.responseText;
+    }
+};
+
+console.log(data);
